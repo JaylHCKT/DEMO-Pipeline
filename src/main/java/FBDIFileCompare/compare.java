571f820816d1a6994e27c1373032e006e9746b30
Jason Lumicao <jason.lumicao@thehackettgroup.com>
2021-11-04 12:22:08 -0700

    Changes in code and directory structure
    Changes in code and directory structure

--- src/main/java/FBDIFileCompare/compare.java
+++ src/main/java/FBDIFileCompare/compare.java
@@ -7,84 +7,104 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class compare {
-	public void comparer() throws Exception {
+	public boolean comparer(String dir) throws Exception {
+		int diff = 0;
 		String[] filenames;
-    	String dir = "./src/main/resources/files/";
     	File f = new File(dir);
         // Populates the array with names of files and directories
         filenames = f.list();
-
-    	File firstFile = new File(dir+filenames[0]);
-        File secondFile = new File(dir+filenames[1]);
-        BufferedReader br1 = null;
-        BufferedReader br2 = null;
-        String sCurrentLine;
-        List<String> list1 = new ArrayList<String>();
-        List<String> list2 = new ArrayList<String>();
-        br1 = Files.newBufferedReader(firstFile.toPath());
-        br2 = Files.newBufferedReader(secondFile.toPath());
-        while ((sCurrentLine = br1.readLine()) != null) {
-        	if(sCurrentLine.length() >= 1) {
-        		if(sCurrentLine.charAt(0) != '-') {            			
-        			if (sCurrentLine.charAt(0) == '(' && sCurrentLine.length() > 1) {
-        				list1.add(sCurrentLine.substring(1, sCurrentLine.length()-1));
-        			}
-        			else {
-        				if(sCurrentLine.charAt(sCurrentLine.length()-1) == ',' || 
-            					sCurrentLine.charAt(sCurrentLine.length()-1) == ')'){
-            				list1.add(sCurrentLine.substring(0, sCurrentLine.length()-1));
-            			}
-        				else {
-        					list1.add(sCurrentLine);
-        				}
-        			}
-        		}
-        	}         	
-        }
-        while ((sCurrentLine = br2.readLine()) != null) {
-        	if(sCurrentLine.length() >= 1) {
-        		if(sCurrentLine.charAt(0) != '-') {            			
-        			if (sCurrentLine.charAt(0) == '(' && sCurrentLine.length() > 1) {
-        				list2.add(sCurrentLine.substring(1, sCurrentLine.length()-1));
-        			}
-        			else {
-        				if(sCurrentLine.charAt(sCurrentLine.length()-1) == ',' || 
-            					sCurrentLine.charAt(sCurrentLine.length()-1) == ')'){
-            				list2.add(sCurrentLine.substring(0, sCurrentLine.length()-1));
-            			}
-        				else {
-        					list2.add(sCurrentLine);
-        				}
-        			}
-        		}
-        	}  
-        }
-        List<String> tmpList = new ArrayList<String>(list1);
-        tmpList.removeAll(list2);
-        if(tmpList.size() <= 0) {
-        	System.out.println("No missing content from first file");
-        }
-        else {
-        	System.out.println("Content from first file missing in second file:");
-            for(int i=0;i<tmpList.size();i++){
-                System.out.println(tmpList.get(i)); //content from first file which is not there		            
-            }
-        }
-
-        System.out.println();
-        tmpList = list2;
-        tmpList.removeAll(list1);
-        if(tmpList.size() <= 0) {
-        	System.out.println("No missing content from second file");
-        }
-        else {
-            System.out.println("Content from second file missing in first file:");
-            for(int i=0;i<tmpList.size();i++){
-                System.out.println(tmpList.get(i)); //content from second file which is not there
-            }
-        }
         
-        return;
+        if(filenames.length%2 != 0) {
+        	System.out.println("Invalid number of files to compare in " +dir+ " directory\n");
+        	return false;
+        }
+        else {
+        	for (int i=0; i < filenames.length; i+=2) {
+                // Print the names of files and directories
+                //System.out.println(filenames[i]);
+                //System.out.println(filenames[i+1]);
+                File firstFile = new File(dir+filenames[i]);
+                File secondFile = new File(dir+filenames[i+1]);
+                BufferedReader br1 = null;
+                BufferedReader br2 = null;
+                String sCurrentLine;
+                List<String> list1 = new ArrayList<String>();
+                List<String> list2 = new ArrayList<String>();
+                br1 = Files.newBufferedReader(firstFile.toPath());
+                br2 = Files.newBufferedReader(secondFile.toPath());
+                while ((sCurrentLine = br1.readLine()) != null) {
+                	if(sCurrentLine.length() >= 1) {
+                		if(sCurrentLine.charAt(0) != '-') {            			
+                			if (sCurrentLine.charAt(0) == '(' && sCurrentLine.length() > 1) {
+                				list1.add(sCurrentLine.substring(1, sCurrentLine.length()-1));
+                			}
+                			else {
+                				if(sCurrentLine.charAt(sCurrentLine.length()-1) == ',' || 
+                    					sCurrentLine.charAt(sCurrentLine.length()-1) == ')'){
+                    				list1.add(sCurrentLine.substring(0, sCurrentLine.length()-1));
+                    			}
+                				else {
+                					list1.add(sCurrentLine);
+                				}
+                			}
+                		}
+                	}         	
+                }
+                while ((sCurrentLine = br2.readLine()) != null) {
+                	if(sCurrentLine.length() >= 1) {
+                		if(sCurrentLine.charAt(0) != '-') {            			
+                			if (sCurrentLine.charAt(0) == '(' && sCurrentLine.length() > 1) {
+                				list2.add(sCurrentLine.substring(1, sCurrentLine.length()-1));
+                			}
+                			else {
+                				if(sCurrentLine.charAt(sCurrentLine.length()-1) == ',' || 
+                    					sCurrentLine.charAt(sCurrentLine.length()-1) == ')'){
+                    				list2.add(sCurrentLine.substring(0, sCurrentLine.length()-1));
+                    			}
+                				else {
+                					list2.add(sCurrentLine);
+                				}
+                			}
+                		}
+                	}  
+                }
+                List<String> tmpList = new ArrayList<String>(list1);
+                tmpList.removeAll(list2);
+                if(tmpList.size() <= 0) {
+                	System.out.println("No missing content from " + filenames[i]);
+                }
+                else {
+                	System.out.println("Content from " +filenames[i]+ " missing in " +filenames[i+1]+ ":");
+                    for(int j=0;j<tmpList.size();j++){
+                        System.out.println(tmpList.get(j).replaceAll("\\s", "")); //content from first file which is not there		            
+                    }
+                    diff++;
+                }
+
+                System.out.println();
+                tmpList = list2;
+                tmpList.removeAll(list1);
+                if(tmpList.size() <= 0) {
+                	System.out.println("No missing content from " + filenames[i+1]);
+                }
+                else {
+                    System.out.println("Content from " +filenames[i+1]+ " missing in " +filenames[i]+ ":");
+                    for(int j=0;j<tmpList.size();j++){
+                        System.out.println(tmpList.get(j).replaceAll("\\s", "")); //content from second file which is not there
+                    }
+                    diff++;
+                }
+                System.out.println();
+            }
+        }
+        if(diff == 0) {
+        	System.out.println("No changes found in " +dir+ " directory\n");
+        	return true;
+        }
+        else {
+        	System.out.println(diff +" failure(s) found in " +dir+ " directory, changes found in control files\n");
+        	return false;
+        } 
 	}
 }
 
